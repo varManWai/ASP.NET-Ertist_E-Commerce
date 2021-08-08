ba55ffc6a1bf4987659c344268e6422b9c3429d8
Siah Xin Ying <siahxy-wm19@student.tarc.edu.my>
2021-08-08 17:59:30 +0800

    Edit and add Artwork

---
+++ AddArtwork.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Ertist
+{
+    public partial class AddArtwork1 : System.Web.UI.Page
+    {
+        //Upload Image
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection con;
+            string strCon = ConfigurationManager.ConnectionStrings["ertistDB"].ConnectionString;
+            con = new SqlConnection(strCon);
+            con.Open();
+
+            //upload 
+            if (FileUpload1.HasFile)
+            {
+                byte[] imgbyte = FileUpload1.FileBytes;
+
+                //string sqlUpdate = "UPDATE ArtWork set [image] = @image WHERE [artID] = @id";
+                string sqlInsert = "INSERT INTO ArtWork (artworkID, name, price, description, picture, stock, available, categoryID) VALUES(@artworkID, @name, @price, @description, @picture, @stock, @available, @categoryID)";
+
+                //SqlCommand cmd = new SqlCommand("INSERT INTO ArtWork(image) VALUES(@image) WHERE [artID] = '4001'");
+
+                //cmd.CommandType = CommandType.Text;
+
+
+                //string query1 = "insert into Courses(CRN,CourseName,StudyLevel,Capacity,Instructor,Credits,Prerequisite) values (@crn, @cursename, @studylevel, @capacity, @instructor, @credits, @prerequesite)";
+
+                SqlCommand cmd = new SqlCommand(sqlInsert, con);
+
+                //insert
+                int artworkID = Convert.ToInt32(artid.Text);
+                string name = txtName.Text;
+                string price = txtPrice.Text;
+                string description = txtDesc.Text;
+                int stock = 1;
+                string available = ddlAvailable.SelectedItem.Text;
+                int painting = 1;
+                int ink = 2;
+                int watercolour = 3;
+
+                cmd.Parameters.AddWithValue("@artworkID", artworkID);
+                cmd.Parameters.AddWithValue("@picture", imgbyte);
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@price", price);
+                cmd.Parameters.AddWithValue("@description", description);
+                cmd.Parameters.AddWithValue("@stock", Convert.ToInt32(stock));
+                cmd.Parameters.AddWithValue("@available", available);
+                if (ddlCategory.SelectedItem.Text.Equals("Painting"))
+                {
+                    cmd.Parameters.AddWithValue("@categoryID", Convert.ToInt32(painting));
+
+                }
+                else if (ddlCategory.SelectedItem.Text.Equals("Ink"))
+                {
+                    cmd.Parameters.AddWithValue("@categoryID", Convert.ToInt32(ink));
+
+                }
+                else if (ddlCategory.SelectedItem.Text.Equals("Watercolour"))
+                {
+                    cmd.Parameters.AddWithValue("@categoryID", Convert.ToInt32(watercolour));
+                }
+
+
+                //add the rest
+
+                cmd.ExecuteNonQuery();
+
+            }
+
+
+            con.Close();
+            Response.Redirect("EditArtwork.aspx");
+        }
+    }
+}
---
+++ EditArtwork.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace Ertist
+{
+    public partial class AddArtwork : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Display Image
+            if (!this.IsPostBack)
+            {
+                string strCon = ConfigurationManager.ConnectionStrings["ertistDB"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(strCon))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM ArtWork", conn))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        Images.DataSource = dt;
+                        Images.DataBind();
+                    }
+                }
+            }
+
+        }
+
+        //Display Image
+        protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                DataRowView dr = (DataRowView)e.Row.DataItem;
+                string imageUrl = "data:image/jpg;base64," + Convert.ToBase64String((byte[])dr["picture"]);
+                (e.Row.FindControl("Image1") as Image).ImageUrl = imageUrl;
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AddArtwork.aspx");
+        }
+    }
+}
