a20c3657764e52daf9fbaa2290bae809241c9bc8
Lai Man Wai <laizoke98@gmail.com>
2021-08-08 11:21:10 +0800

    Navbar Done

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -33,15 +33,6 @@ namespace Ertist
         protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
 
         /// <summary>
-        /// ImageButton2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton ImageButton2;
-
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
