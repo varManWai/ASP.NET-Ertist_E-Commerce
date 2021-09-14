6ba2ca02fde5f0194206f441274ad5dbd8106c53
Lai Man Wai <laizoke98@gmail.com>
2021-09-15 03:55:15 +0800

    OrderSuccess, Payment

---
+++ OrderSuccess.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace Ertist
+{
+    public partial class OrderSuccess : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            if (!IsPostBack)
+            {
+                sendEmail();
+            }
+        }
+        public static void UpdateArtwork(int productId)
+        {
+
+            string sql = @"UPDATE Artwork SET available = @available, stock = @stock FROM ARTWORK where artworkID = @artworkID";
+            string strCon = ConfigurationManager.ConnectionStrings["ertistDB"].ConnectionString;
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            cmd.Parameters.AddWithValue("@available", "Not Available");
+            cmd.Parameters.AddWithValue("@stock", 0);
+            cmd.Parameters.AddWithValue("@artworkID", productId);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+        public void sendEmail()
+        {
+
+            string emailSender = ConfigurationManager.AppSettings["username"].ToString();
+            string emailSenderPassword = ConfigurationManager.AppSettings["password"].ToString();
+            string emailSenderHost = ConfigurationManager.AppSettings["smtp"].ToString();
+            int emailSenderPort = Convert.ToInt16(ConfigurationManager.AppSettings["portnumber"]);
+            Boolean emailIsSSL = Convert.ToBoolean(ConfigurationManager.AppSettings["IsSSL"]);
+
+
+            //Fetching Email Body Text from EmailTemplate File.  
+            string FilePath = "C:\\Users\\USER\\source\\repos\\XinyiLoh\\Ertist\\Email.html";
+            StreamReader str = new StreamReader(FilePath);
+            string MailText = str.ReadToEnd();
+            str.Close();
+
+            //Repalce [newusername] = signup user name   
+            MailText = MailText.Replace("[Username]", Convert.ToString(Session["username"]));
+
+            MailText = MailText.Replace("[orderID]", Convert.ToString(Session["email"]));
+            MailText = MailText.Replace("[orderDate]", Convert.ToString(Session["email"]));
+
+            MailText = MailText.Replace("[Total]", "$" + Convert.ToString(Session["payment"]));
+
+
+
+
+            string subject = "Ordered Seccuesfully";
+
+            //Base class for sending email  
+            MailMessage _mailmsg = new MailMessage();
+
+            //Make TRUE because our body text is html  
+            _mailmsg.IsBodyHtml = true;
+
+            //Set From Email ID  
+            _mailmsg.From = new MailAddress(emailSender);
+
+            //Set To Email ID  
+            _mailmsg.To.Add((string)Session["email"]);
+
+            //Set Subject  
+            _mailmsg.Subject = subject;
+
+            //Set Body Text of Email   
+            _mailmsg.Body = MailText;
+
+
+            //Now set your SMTP   
+            SmtpClient _smtp = new SmtpClient();
+
+            //Set HOST server SMTP detail  
+            _smtp.Host = emailSenderHost;
+
+            //Set PORT number of SMTP  
+            _smtp.Port = emailSenderPort;
+
+            //Set SSL --> True / False  
+            _smtp.EnableSsl = emailIsSSL;
+
+            //Set Sender UserEmailID, Password  
+            NetworkCredential _network = new NetworkCredential(emailSender, emailSenderPassword);
+            _smtp.Credentials = _network;
+
+            //Send Method will send your MailMessage create above.  
+            _smtp.Send(_mailmsg);
+        }
+    }
+}
